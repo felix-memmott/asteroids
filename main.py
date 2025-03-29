cb3b05c0e5ee2e1397c9fb62c694b79b4b401967
felix-memmott <felix.memmott@gmail.com>
2025-03-29 17:46:45 +0000

    added basic game loop

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    # initialize pygame
+    pygame.init()
 
+    # set up the display
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+
+    # main loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # update the display
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+ 
+    # clean up
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
