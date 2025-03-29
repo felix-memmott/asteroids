3722e56a578ee03591a288577c99390ac5f5f404
felix-memmott <felix.memmott@gmail.com>
2025-03-29 17:49:26 +0000

    added game clock to main

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     # initialize pygame
     pygame.init()
 
+    # set up the clock
+    clock = pygame.time.Clock()
+    dt = 0
+
     # set up the display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
@@ -15,6 +19,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        # update the game state
+        dt = clock.tick(60) / 1000
 
         # update the display
         screen.fill((0, 0, 0))
